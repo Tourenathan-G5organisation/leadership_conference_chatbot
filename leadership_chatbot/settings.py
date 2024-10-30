79ef28d23b552a26ce3df72f9bd91f1c4195fb19
teyounathan <toure21@live.com>
2024-10-30 10:08:06 +0100

    modified configs

--- leadership_chatbot/settings.py
+++ leadership_chatbot/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gppwjiw^6g9t7#==x@tju()l5jftxcc2xg_)eqa%d#%oxspx+)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['clc2024.azurewebsites.net', '127.0.0.1']
 
 
 # Application definition
